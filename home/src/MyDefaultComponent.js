d57d0638151f2caa3e2b8167e048024b7f1e82c4
Satish Rajnale <76589507+satish-rajnale@users.noreply.github.com>
2022-01-13 18:17:16 +0530

    default compo import

---
+++ home/src/MyDefaultComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyDefaultComponent() {
+    return (
+        <div>
+            Default is here
+        </div>
+    )
+}
+
+export default MyDefaultComponent
